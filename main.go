a729bac40ab81d21f588fe0422728544cb2275ff
Juan J Chorro Bergillo <jjchorrobe@gmail.com>
2022-04-11 14:28:04 +0200

    feat: add main

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/chorro/openesp/repository"
+	"github.com/chorro/openesp/service"
+)
+
+func main() {
+	memoryRepository := repository.NewMemoryDeviceRepository()
+
+	deviceService := service.NewDeviceRule(memoryRepository)
+
+	deviceService.CreateDevice("my-device")
+	if err := deviceService.CreateDevice("too-long-device-name."); err != nil {
+		fmt.Println("error: ", err)
+	}
+
+	devices, _ := deviceService.GetDevices()
+	fmt.Println(devices)
 }
